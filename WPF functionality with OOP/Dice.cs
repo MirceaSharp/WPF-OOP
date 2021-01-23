f938f1fbdfb38a7e2a062938fcfb2d48949edd6a
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-01-23 18:43:12 +0100

    Initial commit

---
+++ WPF functionality with OOP/Dice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WPF_functionality_with_OOP
+{
+    class Dice
+    {
+        private int _numberOfSides;
+        private int _value;
+        private Random _randomNumber;
+
+        public int NumberOfSides { get; set; }
+        
+            
+
+
+        public int Value { get; set; }
+        
+
+        public Random RandomNumber { get; set; }
+
+        public Dice(int numberOfSides, Random r)
+        {
+            NumberOfSides = numberOfSides;
+            Value = 1;
+            RandomNumber = r;
+
+        }
+
+        public Dice(Random r)
+        {
+            NumberOfSides = 6;
+            Value = 1;
+            RandomNumber = new Random();
+
+        }
+
+
+        public void Roll()
+        {
+            Value = RandomNumber.Next(1, NumberOfSides + 1);
+        }
+
+
+
+
+    }
+}
---
+++ WPF functionality with OOP/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_functionality_with_OOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dice Dice = new Dice(6, new Random());
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Dice.Roll();
+            txt1.Content = Dice.Value.ToString();
+            Dice.Roll();
+            txt2.Content = Dice.Value.ToString();
+        }
+    }
+}
